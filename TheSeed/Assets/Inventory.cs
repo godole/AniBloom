100107e4806741013d18bf309a136304b5f42eed
GODOLE\Godole-user <godole199@naver.com>
2017-05-05 22:53:57 +0900

    asdfds

---
+++ TheSeed/Assets/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+    private List<string> m_lItemNames = new List<string>();
+
+    private List<string> m_ItemNames = new List<string>();
+
+    private List<UISprite> m_Sprites = new List<UISprite>();
+
+    public GameObject m_gObjSampleItem;
+
+    public UIScrollView m_scrollView;
+
+    public UITable m_grid;
+
+	// Use this for initialization
+	void Start () {
+        m_lItemNames.Add("material1");
+        m_lItemNames.Add("material2");
+        m_lItemNames.Add("material3");
+        m_lItemNames.Add("material4");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I))
+            AddItem();
+	}
+
+    private void AddItem()
+    {
+        int nRandomIndex = Random.Range(0, m_lItemNames.Count);
+
+        GameObject gObjItem = NGUITools.AddChild(m_grid.gameObject, m_gObjSampleItem);
+
+        UISprite itemScript = gObjItem.GetComponent<UISprite>();
+
+        itemScript.spriteName = m_lItemNames[nRandomIndex];
+
+        m_grid.Reposition();
+
+        m_scrollView.ResetPosition();
+    }
+}
---
+++ TheSeed/Assets/MainLogic.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class MainLogic : MonoBehaviour {
+    public GameObject _PlanetPrefab = null;
+
+    public GameObject _Cosmos;
+
+    public List<GameObject> _PlanetList = new List<GameObject>();
+
+    
+    public Vector2 _WorldSize;
+
+    void Awake()
+    {
+        try
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                var planet = Instantiate(_PlanetPrefab);
+                planet.transform.SetParent(_Cosmos.transform);
+                float scale = UnityEngine.Random.Range(200, 500);
+                planet.transform.localScale = new Vector3(scale, scale, 100);
+                planet.transform.position = new Vector3(UnityEngine.Random.Range(-_WorldSize.x / 2, _WorldSize.x / 2), 
+                    UnityEngine.Random.Range(-_WorldSize.y / 2, _WorldSize.y / 2), -10);
+                _PlanetList.Add(planet);
+            }
+        }
+        catch(NullReferenceException e)
+        {
+            Debug.Log("Create Planet Error : " + e.Message);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
